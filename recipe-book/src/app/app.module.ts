f40fd6a781566474d9d9884657228c308b5ea5a6
Adam <fuzumoe@gmail.com>
2016-09-28 04:03:31 +0200

    ..........

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { RecipeBookAppComponent } from "./recipe-book.component";
-import { HeaderComponent } from "./header.component";
-import { ShoppingListAddComponent } from "./shopping-list/shopping-list-add.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item.component";
+import { RecipeBookAppComponent } from './recipe-book.component';
+import { HeaderComponent } from './header.component';
+import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
